ec8ac5c654292762ce22049760aa19ae6cd1f61b
Naufal Massawa Lubis <naufalmassawa@gmail.com>
2025-07-20 00:55:08 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello, World!';
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
