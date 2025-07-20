e20feb37797d36f56e366a93314eb8bd4f121a41
Naufal Massawa Lubis <naufalmassawa@gmail.com>
2025-07-20 19:03:45 +0700

    modified server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const albums = require('./api/albums');
+const songs = require('./api/songs');
+const AlbumsService = require('./services/postgres/AlbumsService');
+const AlbumsValidator = require('./validator/albums');
+const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const server = Hapi.server({
@@ -13,12 +20,51 @@ const init = async () => {
     },
   });
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Hello, World!';
+  // Register albums plugin
+  await server.register({
+    plugin: albums,
+    options: {
+      service: AlbumsService,
+      validator: AlbumsValidator,
     },
+  });
+
+  // Register songs plugin
+  await server.register({
+    plugin: songs,
+    options: {
+      service: SongsService,
+      validator: SongsValidator,
+    },
+  });
+
+  // Global error handling
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    // Handle custom client error
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    // Handle internal server error
+    if (response.isBoom) {
+      console.error(response);
+
+      const newResponse = h.response({
+        status: 'error',
+        message: 'Terjadi kesalahan pada server',
+      });
+      newResponse.code(500);
+      return newResponse;
+    }
+
+    return h.continue;
   });
 
   await server.start();
